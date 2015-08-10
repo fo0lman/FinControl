bc194eea8098a561f0683bb2c479c6b5caa8405e
tryfonkov <trifonkov@gmail.com>
2015-08-10 15:51:07 +0300

    #10 Yuriy Authenticate

--- dist/js/views/pages/RegistrationPage.js
+++ dist/js/views/pages/RegistrationPage.js
@@ -1,15 +1,32 @@
 define([
     'views/pages/AbstractPage',
     'handlebars',
-    'templates'
-], function (AbstractPage, Handlebars, JST) {
+    'templates',
+    'modules/authorization'
+], function (AbstractPage, Handlebars, JST, UserModule) {
     "use strict";
 
     var RegistrationPageView;
     RegistrationPageView = AbstractPage.extend({
         title: 'Registration',
+
+        events: {
+            'submit':  'regUser'
+        },
+
         initialize: function() {
             this.template = Handlebars.compile(JST.RegistrationPage({title: this.title}));
+        },
+
+        regUser: function( event ) {
+            var email,
+                password;
+
+            email = this.$el.find('#inputEmail').val();
+            password = this.$el.find('#inputPassword').val();
+            event.preventDefault();
+            UserModule.regUser(email, password);
+            router.navigate('dashboard', {trigger: true});
         }
     });
 
