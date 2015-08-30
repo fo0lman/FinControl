da2770de3ef7dc22d87ffab9f12f0031be672d8a
AObolonkova <aleksandraObolonkova@yandex.ru>
2015-08-30 23:50:32 +0300

    Aleksandra sort and filter Statistic Page

--- dist/js/views/ItemsView.js
+++ dist/js/views/ItemsView.js
@@ -17,6 +17,8 @@ define([
             this.listenTo(this.collection, 'sync update', this.render);
             this.listenTo(this.collection, 'update', this.checkListSize);
             this.listenTo(this.collection, 'sync', this.spinnerStop);
+
+            this.listenTo(this.collection, "sort", this.render);
         },
         renderItem: function (item) {
             var itemView = new ItemView({
@@ -27,6 +29,12 @@ define([
         render: function() {
             this.template = Handlebars.compile(JST.Items());
             this.$el.html(this.template());
+            this.delegateEvents({
+                'click thead tr': 'sortByDate',
+                'click thead tr': 'sortBySum',
+                'click thead tr': 'sortByCategory',
+                'click thead tr': 'sortBySource'
+            });
             var lastTenModelsArr = this.collection.last(10);
             lastTenModelsArr.forEach(this.renderItem, this);
             return this;
@@ -41,7 +49,19 @@ define([
         },
         spinnerStop: function () {
             spinner.show(false);
-
+        },
+        
+        sortByDate: function(){
+            this.collection.byDate();
+        },
+        sortBySum: function(){
+            this.collection.bySum();
+        },
+        sortByCategory: function(){
+            this.collection.byCategory();
+        },
+        sortBySource: function(){
+            this.collection.bySource();
         }
     });
 
--- dist/js/views/StatisticsFilterView.js
+++ dist/js/views/StatisticsFilterView.js
@@ -1,8 +1,9 @@
 define([
     'backbone',
     'handlebars',
-    'templates'
-], function (Backbone, Handlebars, JST) {
+    'templates',
+    'collections/items/ItemsControl'
+], function (Backbone, Handlebars, JST, ItemsControl) {
     "use strict";
 
     var StatisticsFilterView;
@@ -13,14 +14,62 @@ define([
         },
         initialize: function () {
             this.template = Handlebars.compile(JST.StatisticsFilter());
+
+            this.collection = ItemsControl.getItemsCollection();
         },
         render: function () {
             this.$el.append(this.template());
             return this;
         },
+        getFilterDateBegin: function () {
+            return $('#inputDateBegin').val();
+        },
+        getFilterDateEnd: function () {
+            return $('#inputDateEnd').val();
+        },
+        getFilterType: function () {
+            return $('#inputType').val();
+        },
+        getFilterCategory: function () {
+            return $('#inputCategory').val();
+        },
+        getFilterSource: function () {
+            return $('#inputSource').val();
+        },
+        getSumFilter: function (type, model) {
+
+            if (type === 'all') {
+                return model.get('sum'); 
+            } else {
+                    if (type === 'income') { 
+                        return model.get('sum') > 0;            
+                    };
+                    if (type === 'costs') { 
+                        return model.get('sum') < 0;            
+                    }; 
+                }
+        },
         filter: function (event) {
             event.preventDefault();
             console.log('filtered!');
+
+            var dateBegin = this.getFilterDateBegin(); 
+            var dateEnd = this.getFilterDateEnd(); 
+            var type = this.getFilterType();
+            var category = this.getFilterCategory();
+            var source = this.getFilterSource();
+
+            var itemCollection = new ItemsControl.getItemsCollection();
+            this.collection.setFilter(category);
+            this.collection.pager();
+
+            var date = itemCollection.get('date');
+            itemCollection.create({
+                category: category,
+                date: date>dateBegin && date<dateEnd,
+                source: source,
+                sum: this.getSumFilter(type, itemCollection)
+            });
         }
     });
 
